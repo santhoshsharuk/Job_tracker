418b69ffb4e5124ac16e24b766f23ee346c2bc9e
santhoshsharuk <santhoshsharuk16@gmail.com>
2025-11-09 23:08:57 +0530

    tracking system

--- index.tsx
+++ index.tsx
@@ -1,7 +1,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Clarity from '@microsoft/clarity';
 import App from './App';
+
+Clarity.init('u3idkeq8dk');
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
